f574f452f2824f67c7f3e6a7b5e525a46cb6bd06
tfb123 <mil.xomiak@gmail.com>
2022-11-27 14:54:43 +0200

    Second Commit

--- app/src/main/java/com/example/apiprogram/MainActivity.kt
+++ app/src/main/java/com/example/apiprogram/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.apiprogram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.apiprogram.databinding.ActivityMainBinding
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fetchCurrencyData().start()
     }
+
+    private fun fetchCurrencyData(): Thread
+    {
+        return Thread {
+            val url = URL("https://v6.exchangerate-api.com/v6/b63d0862c333128cdf156ac0/latest/USD")
+            val connection = url.openConnection() as HttpsURLConnection
+
+            if (connection.responseCode == 200)
+            {
+                val inputSystem = connection.inputStream
+                println(inputSystem.toString())
+            }
+            else
+            {
+                binding.baseCurrency.text = "Failed Connection"
+            }
+        }
+    }
+
 }
